d08ea8e9e989bb5da0ead59dacbe584cbc78d9c9
AnaSousa <anasousa314@outlook.com>
2022-12-04 17:02:07 -0300

    aula09-criando validators

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const Auth = require('./middlewares/Auth');
 
+const AuthValidator = require('./validators/AuthValidator');
+
 const AuthController = require('../src/controllers/AuthController');
 const UserController = require('../src/controllers/UserController');
 const AdsController = require('../src/controllers/AdsController');
@@ -13,8 +15,8 @@ router.get('/ping', (req,res)=>{
 
 router.get('/states', UserController.getStates);
 
-router.post('/user/signin', AuthController.signin);
-router.post('/user/signup', AuthController.signup);
+router.post('/user/signin',AuthController.signin);
+router.post('/user/signup', AuthValidator.signup, AuthController.signup);
 
 router.get('/user/me', Auth.private, UserController.info);
 router.put('/user/me', Auth.private, UserController.editAction);
